c31715df78fbba7fa1a4b4f8a0e7f9bfb6c4b468
Pierre Jungling <pierrejungling@gmail.com>
2026-02-02 15:09:32 +0100

    maj url

--- frontend/src/environment/environment.prod.ts
+++ frontend/src/environment/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: '/api/',
+  apiURL: 'https://gravisterie-app-backend.onrender.com/api/',
   PARAM_IS_MISSING: 499,
   TOKEN_KEY: 'token'
 };
